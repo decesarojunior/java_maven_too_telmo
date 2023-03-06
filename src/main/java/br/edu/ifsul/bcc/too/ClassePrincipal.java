f220c9d82b0e73096b6a29a78420e4195e715d03
decesarojunior <decesarojunior@yahoo.com.br>
2023-03-06 11:06:42 -0300

    implementacoes em 06/03/2023

---
+++ src/main/java/br/edu/ifsul/bcc/too/ClassePrincipal.java
@@ -0,0 +1,14 @@
+
+
+package br.edu.ifsul.bcc.too;
+
+/**
+ *
+ * @author telmo
+ */
+public class ClassePrincipal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/br/edu/ifsul/bcc/too/topico3/exercicios/Teste.java
@@ -0,0 +1,27 @@
+package br.edu.ifsul.bcc.too.topico3.exercicios;
+
+/**
+ *
+ * @author telmo
+ * comentário de bloco
+ */
+
+public class Teste {
+    
+    //método main.
+    public static void main(String args[]){
+        
+     //comentario de linha   
+     
+     //exercicio 27/02/2023
+        
+     // Comando para compilação (a partir do diretorio java): javac br/edu/ifsul/bcc/too/base/topico3/exercicios/Teste.java
+    
+     // Comando para execução (a partir do diretorio java): java br/edu/ifsul/bcc/too/base/topico3/exercicios/Exercicio4
+ 
+        System.out.println("teste 123 456");
+        
+    }
+    
+    
+}
